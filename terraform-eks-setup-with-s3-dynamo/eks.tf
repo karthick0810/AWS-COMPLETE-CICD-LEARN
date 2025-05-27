12480c75f3990c13c2d360eaf2c84ea6d7fe59f7
karthick.d <karthick.d@kgisl.com>
2025-05-27 16:55:29 +0530

    updated eks

--- terraform-eks-setup-with-s3-dynamo/eks.tf
+++ terraform-eks-setup-with-s3-dynamo/eks.tf
@@ -1,17 +1,20 @@
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
+  version         = "20.36.0"
+
   cluster_name    = var.cluster_name
   cluster_version = "1.29"
-  subnets         = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
+  subnet_ids      = module.vpc.private_subnets
   enable_irsa     = true
 
-  node_groups = {
+  eks_managed_node_groups = {
     eks_nodes = {
-      desired_capacity = 2
-      max_capacity     = 3
-      min_capacity     = 1
-      instance_type    = "t3.medium"
+      desired_size   = 2
+      max_size       = 3
+      min_size       = 1
+      instance_types = ["t3.medium"]
+      capacity_type  = "ON_DEMAND"
     }
   }
 }
