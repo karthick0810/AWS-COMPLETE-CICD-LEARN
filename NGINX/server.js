a846acb07b1910ba9327b3d632b0a2d618ebfe93
karthick.d <karthick.d@kgisl.com>
2025-05-27 14:58:55 +0530

    nginx updated

--- NGINX/server.js
+++ NGINX/server.js
@@ -1,25 +1,24 @@
 const express = require('express');
+const morgan = require('morgan');  // <-- add this
 
 const app = express();
 
 require('dotenv').config();
 
-
 app.set('view engine','ejs');
 app.set('views','./views');
 
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname + '/public'));
 
+// Add morgan middleware for logging requests
+app.use(morgan('combined'));
 
-app.get("/",function(req,res) {
+app.get("/", function(req, res) {
     res.render("index");
 })
 
-
 const PORT = process.env.PORT;
 
-
-
-app.listen(PORT,() => {
+app.listen(PORT, () => {
     console.log(`app is listening to port ${PORT}`);
-})
+});
