08131a02edb25db29e8d8c7d3270bfce6f62e839
karthick.d <karthick.d@kgisl.com>
2025-05-27 15:53:22 +0530

    updated

--- terraform-eks-setup-with-s3-dynamo/backend.tf
+++ terraform-eks-setup-with-s3-dynamo/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-eks-terraform-state-bucket9081"
+    bucket         = "my-eks-terraform-state-bucket9082"
     key            = "eks/terraform.tfstate"
     region         = "us-east-2"
     dynamodb_table = "terraform-locks"
