aea34a8f3705173ab2e79b0e22939e7fdbe1fba3
karthick.d <karthick.d@kgisl.com>
2025-05-29 11:03:15 +0530

    updated

--- Build.sh
+++ Build.sh
@@ -3,12 +3,12 @@ aws ecr get-login-password --region us-east-2 | docker login --username AWS --pa
 
 cd JAVA
 # Build, tag, push karthickapp image
-docker build -t karthickapp .
+docker build --no-cache -t karthickapp .
 docker tag karthickapp:latest 754450290666.dkr.ecr.us-east-2.amazonaws.com/karthickapp:latest
 docker push 754450290666.dkr.ecr.us-east-2.amazonaws.com/karthickapp:latest
 cd ../NGINX
 # Build, tag, push nginx image
-docker build -t nginx .
+docker build --no-cache -t nginx .
 docker tag nginx:latest 754450290666.dkr.ecr.us-east-2.amazonaws.com/nginx:latest
 docker push 754450290666.dkr.ecr.us-east-2.amazonaws.com/nginx:latest
 
